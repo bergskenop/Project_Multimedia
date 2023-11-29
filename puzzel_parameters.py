1fa62dc455b91729ecb9f8097feb4f4535a155b4
bryvdnbe <bryanvandenberghe@hotmail.com>
2023-11-29 12:47:25 +0100

    edit segmentation.py

--- puzzel_parameters.py
+++ puzzel_parameters.py
@@ -6,19 +6,19 @@ from tkinter import filedialog
 
 
 def bepaal_puzzel_parameters(image_path):
-    image = plt.imread(path)
+    image = plt.imread(image_path)
     plt.imshow(image)
     plt.show()
 
     # 1 = shuffled, 2 = scrambled and 3 = rotated
     type_puzzle = 1
-    if re.search(".+_scrambled_.+", path):
+    if re.search(".+_scrambled_.+", image_path):
         type_puzzle = 2
-    elif re.search(".+_rotated_.+", path):
+    elif re.search(".+_rotated_.+", image_path):
         type_puzzle = 3
 
     # Bepaal aantal rijen en kolommen
-    scale = re.compile("[0-9][x][0-9]").findall(path)
+    scale = re.compile("[0-9][x][0-9]").findall(image_path)
     rijen = int(str(re.compile("^[0-9]").findall(scale[0])[0]))
     kolommen = int(str(re.compile("[0-9]$").findall(scale[0])[0]))
 
--- segmentation.py
+++ segmentation.py
@@ -2,23 +2,21 @@ import cv2
 import numpy as np
 from tkinter import filedialog
 
+def segment_pieces(path):
+    img = cv2.imread(path)
+    cv2.imshow('image cut top', img[:img.shape[0] // 2])
+    cv2.imshow('image cut bottom', img[img.shape[0] // 2:])
+    cv2.waitKey(0)
 
+    imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-
-img = cv2.imread('data/Jigsaw_rotated/jigsaw_rotated_2x2_01.png')
-cv2.imshow('image cut top', img[:img.shape[0] // 2])
-cv2.imshow('image cut bottom', img[img.shape[0] // 2:])
-cv2.waitKey(0)
-
-imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-ret, thresh = cv2.threshold(imgray, 0, 250, 0)
-contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours = sorted(contours, key=cv2.contourArea, reverse=True)
-cv2.drawContours(img, contours, 0, (0, 255, 0), 3)
-cv2.drawContours(img, contours, 1, (255, 0, 0), 3)
-cv2.drawContours(img, contours, 2, (255, 255, 0), 3)
-cv2.drawContours(img, contours, 3, (0, 255, 255), 3)
-print(contours[0][0])
-cv2.imshow('contours', img)
-cv2.waitKey(0)
+    ret, thresh = cv2.threshold(imgray, 0, 250, 0)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    cv2.drawContours(img, contours, 0, (0, 255, 0), 3)
+    cv2.drawContours(img, contours, 1, (255, 0, 0), 3)
+    cv2.drawContours(img, contours, 2, (255, 255, 0), 3)
+    cv2.drawContours(img, contours, 3, (0, 255, 255), 3)
+    print(contours[0][0])
+    cv2.imshow('contours', img)
+    cv2.waitKey(0)
