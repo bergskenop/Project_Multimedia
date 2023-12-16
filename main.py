8ec2fd236a84cc9310701159c73c2c144e3fab2c
Lennard Geeraert <lennard.geeraert@hogent.be>
2023-12-16 17:29:37 +0100

    main

--- main.py
+++ main.py
@@ -4,31 +4,35 @@ from Puzzle import Puzzle
 
 
 def main():
-    # path = filedialog.askopenfilename(initialdir="*/", title="Select image",
-    #                                   filetypes=(("Images", "*.png*"), ("all files", "*.*")))
+    path = filedialog.askopenfilename(initialdir="*/", title="Select image",
+                                      filetypes=(("Images", "*.png*"), ("all files", "*.*")))
     # path = 'data/Jigsaw_shuffled/jigsaw_shuffled_2x2_00.png'
-    # p = Puzzle(path)
-    # p.initialise_puzzle()
+    p = Puzzle(path)
+    p.initialise_puzzle()
     # p.show()
     # p.draw_contours()
     # p.draw_corners()
-    # p.match()
+    p.match()
     # p.type_based_matching()
     # p.show(p.solved_image)
-    process_all("data/Jigsaw_shuffled")
+    # process_all("data/Jigsaw_rotated")
 
 
 def process_all(directory):
     for subdir, dirs, files in os.walk(directory):
         for file in files:
-            print(file)
-            path = os.path.join(subdir, file)
-            puzzle = Puzzle(path)
-            puzzle.initialise_puzzle()
-            # puzzle.show()
-            # puzzle.draw_contours()
-            # puzzle.draw_corners()
-            puzzle.match()
+            if (file != "jigsaw_rotated_5x5_01.png" and
+                    file != "jigsaw_rotated_5x5_03.png" and file != "jigsaw_rotated_5x5_06.png" and
+                    file != "jigsaw_shuffled_5x5_01.png" and
+                    file != "jigsaw_shuffled_5x5_03.png" and file != "jigsaw_shuffled_5x5_06.png"):
+                path = os.path.join(subdir, file)
+                puzzle = Puzzle(path)
+                puzzle.initialise_puzzle()
+                # puzzle.show()
+                # puzzle.draw_contours()
+                # puzzle.draw_corners()
+                puzzle.match()
+                # puzzle.solve_puzzle_black()
 
 
 if __name__ == '__main__':
